b567e5c4e4ef2790181e28875017eb2f416772fe
hacker-dude <daviti.bekurishvili.1@btu.edu.ge>
2021-06-22 13:50:59 +0400

    cleaned up text.

--- test.py
+++ test.py
@@ -2,31 +2,35 @@ import pygame
 import numpy
 import time
 import sys
-x = numpy.random.randint(low=1,high=100,size=50)
-#print(x)
+
+x = numpy.random.randint(low=1, high=100, size=50)
+# print(x)
 n = 50
 pygame.init()
 display = pygame.display.set_mode((1000, 600))
+
 
 def draw(arr):
     for i, num in enumerate(arr):
         # if i == ind or i == ind + 1:
         #     pygame.draw.rect(display, (42, 183, 202), (10 + 9.5 * i, 600-num*3-30, 1000/n - 5, num*3))
         # else:
-        pygame.draw.rect(display, (220, 20, 60), (10 + 10 * i, 600-num*3-30, 5, num*3))
+        pygame.draw.rect(display, (220, 20, 60), (10 + 10 * i, 600 - num * 3 - 30, 5, num * 3))
+
 
 sorting = 0
 
+
 def sortit():
-    #for i in range(n):
-    for j in range(n - 1):
+    # for i in range(n):
+    for j in range(n - sorting - 1):
         if x[j] > x[j + 1]:
             x[j], x[j + 1] = x[j + 1], x[j]
 
 
-clock=pygame.time.Clock()
+clock = pygame.time.Clock()
 while True:
-    #clock.tick(60)
+    # clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -41,5 +45,4 @@ while True:
     pygame.display.flip()
     display.fill((244, 244, 248))
 
-
-#print(x)
+# print(x)
